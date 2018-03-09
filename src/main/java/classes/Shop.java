cfc162a5c6ec2d119f6b65aa75901ea0bef8da2d
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-09 17:58:56 +0000

    Add TestShop with test for shop getters

--- src/main/java/classes/Shop.java
+++ src/main/java/classes/Shop.java
@@ -6,9 +6,23 @@ import java.util.ArrayList;
 
 public class Shop {
 
+    private String name;
     private ArrayList<Sellable> stock;
 
-    public Shop(ArrayList<Sellable> stock) {
+    public Shop(String name, ArrayList<Sellable> stock) {
+        this.name = name;
         this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Sellable> getStock() {
+        return stock;
+    }
+
+    public void addProductToStock(Sellable product) {
+        this.stock.add(product);
     }
 }
--- src/test/java/classes/TestShop.java
+++ src/test/java/classes/TestShop.java
@@ -1,18 +1,36 @@
 package classes;
 
+import classes.instrument_enums.GuitarType;
+import enums.InstrumentType;
 import interfaces.Sellable;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestShop {
 
     ArrayList<Sellable> stock;
     Shop shop;
+    Guitar guitar1;
 
     @Before
     public void setUp() {
+        guitar1 = new Guitar("Gibson", "ES-339 Studio Ginger Burst (2016)", 1200.00, 1519.00, InstrumentType.GUITAR, GuitarType.ELECTRIC, 6);
         stock = new ArrayList<>();
-        shop = new Shop(stock);
+        shop = new Shop("Jeff's Gear", stock);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Jeff's Gear", shop.getName());
+    }
+
+    @Test
+    public void canGetStockList() {
+        shop.addProductToStock(guitar1);
+        assertEquals(stock, shop.getStock());
     }
 }
